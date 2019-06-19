f410aec96615a1023f2b7622c7267ee2225b2157
June Hanabi <junehanabi@gmail.com>
2019-06-19 15:23:08 -0400

    Manually edited and moved Alt+F4 to UI file

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,8 +4,4 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
 {
     ui.setupUi(this);
-
-    // Funnily enough we can't enter this into the shortcut capturer
-    // Because it exits all of Qt lol
-    ui.actionExit->setShortcut(Qt::ALT + Qt::Key_F4);
 }
