5ebc31cd1901c3f8cabfa1cc07e5d5a6373070ca
June Hanabi <junehanabi@gmail.com>
2019-06-22 20:57:57 -0400

    Some rough WIP on the store

--- src/store/pokemondatabase.cpp
+++ src/store/pokemondatabase.cpp
@@ -1,6 +1,34 @@
 #include "pokemondatabase.h"
 
-PokemonDatabase::PokemonDatabase(QObject *parent) : QObject(parent)
-{
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QByteArray>
 
+PokemonDatabase::PokemonDatabase(QObject *parent) :
+    QObject(parent)
+{
+    // Read In Json Data to array list
+//    this->jsonItems = this->jsonToList("qrc:/data/items.json");
+//    this->jsonMoves = this->jsonToList("qrc:/data/moves.json");
+//    this->jsonPokemon = this->jsonToList("qrc:/data/pokemon.json");
+//    this->jsonTypes = this->jsonToList("qrc:/data/types.json");
 }
+
+//QVariantList PokemonDatabase::jsonToList(QString filename)
+//{
+//    // Prepare to read in data
+//    QByteArray val;
+//    QFile file;
+
+//    // Read in file
+//    file.setFileName(filename);
+//    file.open(QIODevice::ReadOnly | QIODevice::Text);
+//    val = file.readAll();
+//    file.close();
+
+//    // Return QVariant List
+//    QJsonDocument doc = QJsonDocument::fromJson(val);
+//    return doc.array()[0].toObject().
+//}
--- src/store/pokemondatabase.h
+++ src/store/pokemondatabase.h
@@ -3,12 +3,28 @@
 
 #include <QtCore/QObject>
 #include <QtCore/qglobal.h>
+#include <QHash>
+#include <QVariant>
+#include <QVariantList>
+
+#include "../model/item.h"
+#include "../model/move.h"
+#include "../model/pokemon.h"
+#include "../model/type.h"
 
 class PokemonDatabase : public QObject
 {
     Q_OBJECT
 public:
     explicit PokemonDatabase(QObject *parent = nullptr);
+
+//    const QHash<QVariant, Item> items;
+//    const QHash<QVariant, Move> move;
+//    const QHash<QVariant, Pokemon> pokemon;
+//    const QHash<QVariant, Type> type;
+
+private:
+    //QVariantList jsonToList(QString filename);
 };
 
 #endif // POKEMONDATABASE_H
