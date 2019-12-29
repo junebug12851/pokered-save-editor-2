27cc1f58f39ab3edd8ecf02f64a3fa7747b9a115
June Hanabi <junehanabi@gmail.com>
2019-12-28 21:04:02 -0500

    Completed EventPokemon, tested it works

---
+++ src/data/eventpokemon.cpp
@@ -0,0 +1,102 @@
+/*
+  * Copyright 2019 June Hanabi
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *   http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+*/
+#include "eventpokemon.h"
+
+#include "./gamedata.h"
+
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonValueRef>
+
+EventPokemonEntry::EventPokemonEntry()
+{
+  otName = new QVector<QString>();
+  moves = new QVector<QString>();
+}
+
+void EventPokemon::load()
+{
+  // Grab Event Pokemon Data
+  auto eventPokemonData = GameData::json("eventPokemon");
+
+  // Go through each event Pokemon
+  for(QJsonValue eventPokemonEntry : eventPokemonData->array())
+  {
+    // Create a new event Pokemon entry
+    auto entry = new EventPokemonEntry();
+
+    // Set simple properties
+    entry->title = eventPokemonEntry["title"].toString();
+    entry->desc = eventPokemonEntry["desc"].toString();
+    entry->pokemon = eventPokemonEntry["pokemon"].toString();
+    entry->region = eventPokemonEntry["region"].toString();
+
+    // Set simple optional proeprties
+    if(eventPokemonEntry["otID"].isString())
+      entry->otId = eventPokemonEntry["otID"].toString().toInt(nullptr, 16);
+
+    if(eventPokemonEntry["level"].isDouble())
+      entry->level = eventPokemonEntry["level"].toDouble();
+
+    // Set Moves
+    auto movesArr = eventPokemonEntry["moves"].toArray();
+    for(QJsonValue moveEntry : movesArr)
+      entry->moves->append(moveEntry.toString());
+
+    // Set OT Name
+    // Can be array or string
+    if(eventPokemonEntry["otName"].isArray())
+    {
+      auto otNameArr = eventPokemonEntry["otName"].toArray();
+
+      for(auto otNameEntry : otNameArr)
+        entry->otName->append(otNameEntry.toString());
+    }
+    else
+      entry->otName->append(eventPokemonEntry["otName"].toString());
+
+    // Set DV
+    if(eventPokemonEntry["dv"].isString())
+    {
+      entry->dv = new QVector<var8>();
+
+      // DV Can be
+      // "max"             All DV Values of 15
+      // ":##:##:##:##"    Specific DV Values
+      //                   Atk, Def, Spd, Sp
+      auto dvVal = eventPokemonEntry["dv"].toString();
+      if(dvVal == "max") {
+        (*entry->dv)->append(15);
+        (*entry->dv)->append(15);
+        (*entry->dv)->append(15);
+        (*entry->dv)->append(15);
+      }
+      else
+      {
+        auto dvValParts = dvVal.split(":", QString::SkipEmptyParts);
+        (*entry->dv)->append(dvValParts[0].toInt(nullptr, 10));
+        (*entry->dv)->append(dvValParts[1].toInt(nullptr, 10));
+        (*entry->dv)->append(dvValParts[2].toInt(nullptr, 10));
+        (*entry->dv)->append(dvValParts[3].toInt(nullptr, 10));
+      }
+    }
+
+    eventPokemon->append(entry);
+  }
+}
+
+QVector<EventPokemonEntry*>* EventPokemon::eventPokemon =
+    new QVector<EventPokemonEntry*>;
---
+++ src/data/eventpokemon.h
@@ -0,0 +1,54 @@
+/*
+  * Copyright 2019 June Hanabi
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *   http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+*/
+#ifndef EVENTPOKEMON_H
+#define EVENTPOKEMON_H
+
+// With amazing help of Quicktype!!!
+// https://app.quicktype.io
+
+#include "../common/types.h"
+#include <variant>
+#include <optional>
+#include <QString>
+#include <QVector>
+
+// Optional
+using OtId =   std::optional<var16>;
+using DV =     std::optional<QVector<var8>*>;
+using Level =  std::optional<var8>;
+
+struct EventPokemonEntry {
+    EventPokemonEntry();
+
+    QString title; // Event Title
+    QString desc; // Event Pokemon Description
+    QString pokemon; // Pokemon Name
+    QVector<QString>* otName; // Pokemon OT Name
+    OtId otId; // Pokemon OT ID, random if not specified
+    DV dv; // Pokemon DV List, random if not specified
+    QString region; // Region Code
+    QVector<QString>* moves; // Move list
+    Level level; // Level, default minimum if not specified
+};
+
+class EventPokemon
+{
+public:
+  static void load();
+  static QVector<EventPokemonEntry*>* eventPokemon;
+};
+
+#endif // EVENTPOKEMON_H
